17d00186ae64455b7114bf82c470c075652ceae0
Brian Laptop <Brian Laptop@DESKTOP-T6NEG44>
2018-03-19 17:56:13 -0600

    Saving Changes

--- SandiaAerospaceShipping/MainWindow.xaml.cs
+++ SandiaAerospaceShipping/MainWindow.xaml.cs
@@ -112,12 +112,12 @@ namespace SandiaAerospaceShipping
 
         public static List<string> Components()
         {
-            List<string> lComponentList = new List<string> { "Safe_128", "Safe_328", "Safe_5285", "ACF_314", "ACF_328", "ACF_528",
+            List<string> lComponentList = new List<string> { "Safe_128", "Safe_328", "Safe_528", "ACF_314", "ACF_328", "ACF_528",
                                                             "SRU_1", "SRU_5","SRU_5_Mod","SRU_10", "ST_26", "ST_32_00", "ST_32_01",
                                                             "AIS_200B_35", "AIS_240B_35", "SA_3", "SA_3_L", "SA_3_NVG", "SA_15", "SA_24",
                                                             "SR_34_1", "SR_54_1", "SR_64_1", "SR_263", "SR_623", "GI_205", "STX_165",
                                                             "STX_165_Remote", "SAE_5_35", "360_PM", "360_Remote", "KI_300", "SAI_340",
-                                                            "305477_00_Kit"};
+                                                            "305477_00_Kit", "ADC_00", "ADC_01", "Panel Punch"};
             return lComponentList;
         }
         #endregion
@@ -185,7 +185,7 @@ namespace SandiaAerospaceShipping
         {
             try
             {
-                string sQuery = "SELECT * FROM Shipping_Log";
+                string sQuery = DatabaseProcedure.OrderingColumns();
                 DataTable dtInfo = DatabaseProcedure.GettingInfoFromDatabase(sQuery);
                 dataGrid.ItemsSource = dtInfo.DefaultView;
             }
@@ -293,7 +293,6 @@ namespace SandiaAerospaceShipping
                 MessageBox.Show(ex.Message.ToString());
             }
         }
-
         public static void BuildingColumns(string pSQLConn)
         {
             FillingOutList();
@@ -363,6 +362,28 @@ namespace SandiaAerospaceShipping
                 catch (Exception ex) { MessageBox.Show(ex.Message.ToString()); }
             }
             return dtInfo;
+        }
+        public static string OrderingColumns()
+        {
+            string sRet = string.Empty;
+            string query1 = $"USE {GettingSettings._sDatabaseName} "+
+                            "SELECT COLUMN_NAME " +
+                            "FROM INFORMATION_SCHEMA.COLUMNS " +
+                            "WHERE TABLE_NAME = 'Shipping_Log' ORDER BY (CASE WHEN Column_Name = 'Log_ID' THEN 0 ELSE 1 END), Column_Name;";
+            DataTable rs = GettingInfoFromDatabase(query1);
+            string query2 = "select";
+            string sep = " ";
+            foreach (DataRow row in rs.Rows)
+            {
+                if (Regex.IsMatch(row["Column_Name"].ToString(), @"^\d"))
+                    query2 += sep + '"' + row["Column_Name"] + '"';
+                else
+                    query2 += sep + row["Column_Name"];
+                sep = ", ";
+            }
+            query2 += $" from Shipping_Log";
+            sRet = query2;
+            return sRet;
         }
 
     }
